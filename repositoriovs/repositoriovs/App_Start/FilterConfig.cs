19bdde1911a9ae74dc753e387999961e02050aab
Samuel Quintero <squinteroagudelo18@gmail.com>
2020-07-31 17:33:38 -0500

    subir repo vs

---
+++ repositoriovs/repositoriovs/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace repositoriovs
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
